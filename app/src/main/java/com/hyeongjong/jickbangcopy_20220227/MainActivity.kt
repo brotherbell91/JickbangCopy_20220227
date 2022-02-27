4aa01bef61c43cd39ded6198e38699ec28bda9be
이형종 <hj36879@naver.com>
2022-02-27 14:45:06 +0900

    어댑터 객체 생성 / 리스트뷰와 연결

--- app/src/main/java/com/hyeongjong/jickbangcopy_20220227/MainActivity.kt
+++ app/src/main/java/com/hyeongjong/jickbangcopy_20220227/MainActivity.kt
@@ -2,11 +2,15 @@ package com.hyeongjong.jickbangcopy_20220227
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyeongjong.jickbangcopy_20220227.adapters.RoomAdapter
 import com.hyeongjong.jickbangcopy_20220227.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRoomList = ArrayList<RoomData>()
+
+    lateinit var mRoomAdapter: RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,9 @@ class MainActivity : AppCompatActivity() {
         mRoomList.add(RoomData(21500,"서울시 강북구",12,"8번째 방입니다."))
         mRoomList.add(RoomData(153000,"서울시 동대문구",7,"9번째 방입니다."))
         mRoomList.add(RoomData(125000,"서울시 동대문구",8,"10번째 방입니다."))
+
+        mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
+        roomListView.adapter = mRoomAdapter
 
 
     }
