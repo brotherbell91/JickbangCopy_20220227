b6cfd065412815b431758772286178758e00df64
이형종 <hj36879@naver.com>
2022-02-27 16:02:57 +0900

    Serializable 보내준 클래스를 받아서 사용  - as RoomData로 원상복구해서 사용

--- app/src/main/java/com/hyeongjong/jickbangcopy_20220227/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/hyeongjong/jickbangcopy_20220227/ViewRoomDetailActivity.kt
@@ -2,12 +2,16 @@ package com.hyeongjong.jickbangcopy_20220227
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hyeongjong.jickbangcopy_20220227.datas.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as RoomData
+
 
 
     }
