d1aba5bdc1cc6ed2e95a1ce422b294ac8b43f1fd
이형종 <hj36879@naver.com>
2022-02-27 14:05:28 +0900

    어댑터 클래스 추가 > ArrayAdapter<RoomData> 상속  - 기본 생성자를 지원 안함.

---
+++ app/src/main/java/com/hyeongjong/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -0,0 +1,9 @@
+package com.hyeongjong.jickbangcopy_20220227.adapters
+
+import android.widget.ArrayAdapter
+import com.hyeongjong.jickbangcopy_20220227.datas.RoomData
+
+class RoomAdapter(
+
+) : ArrayAdapter<RoomData>() {
+}
