25cefa5173b200fa0c4a803f9bd4b6cc43f1c279
이형종 <hj36879@naver.com>
2022-02-27 14:08:19 +0900

    어댑터 클래스의 생성자 수정, 부모클래스의 생성자에 변수들 전달

--- app/src/main/java/com/hyeongjong/jickbangcopy_20220227/adapters/RoomAdapter.kt
+++ app/src/main/java/com/hyeongjong/jickbangcopy_20220227/adapters/RoomAdapter.kt
@@ -1,9 +1,12 @@
 package com.hyeongjong.jickbangcopy_20220227.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.hyeongjong.jickbangcopy_20220227.datas.RoomData
 
 class RoomAdapter(
-
-) : ArrayAdapter<RoomData>() {
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<RoomData>
+) : ArrayAdapter<RoomData>(mContext, resId, mList) {
 }
