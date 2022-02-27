db662257904e5516cb3701d7421ca76756016cdb
이형종 <hj36879@naver.com>
2022-02-27 14:03:03 +0900

    방 데이터 표현용 클래스 생성 (수정)  - 생성자를 수정, 4개의 멤버변수 설정

--- app/src/main/java/com/hyeongjong/jickbangcopy_20220227/datas/RoomData.kt
+++ app/src/main/java/com/hyeongjong/jickbangcopy_20220227/datas/RoomData.kt
@@ -2,10 +2,12 @@ package com.hyeongjong.jickbangcopy_20220227.datas
 
 //하나의 방 -> 가격, 행정구역, 층수, 설명문구 멤버변수로.
 
-class RoomData {
-
+class RoomData(
     val price: Int,
     val address: String,
     val floor: Int,
     val description: String,
+) {
+
+
 }
